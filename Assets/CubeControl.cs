2eb66c724fd0dad08b35f341e8bf1c71c53ff7a9
赤燭包子 <huw12313212@gmail.com>
2016-04-17 03:37:31 +0800

    +Practice 1

---
+++ Assets/CubeControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour 
+{
+	Transform myTransform;
+	float speed = 10;
+
+	void Start () 
+	{
+		myTransform = this.GetComponent<Transform> ();
+		//Debug.Log ("Hello Start");
+	}
+
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			myTransform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			myTransform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Debug.Log ("Mouse Clicked on:"+Input.mousePosition);
+		}
+	}
+
+
+}
