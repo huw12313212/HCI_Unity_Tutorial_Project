3f1612120f155a73066796c8296b8e3447b7eafa
赤燭包子 <huw12313212@gmail.com>
2016-04-17 19:19:54 +0800

    +Practice2

---
+++ Assets/HowieController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowieController : MonoBehaviour 
+{
+	public Transform myTransform;
+	public Rigidbody myRigibody;
+	public float speed = 10;
+	public float jumpSpeed = 10;
+
+	public GameObject boomPrefab;
+
+	public void Boom()
+	{
+		GameObject obj = GameObject.Instantiate (boomPrefab);
+		obj.transform.position = myTransform.position;
+	}
+
+	bool onGround = false;
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		Boom ();
+		onGround = true;
+	}
+
+	void OnCollisionExit(Collision collision) 
+	{
+		onGround = false;
+	}
+
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			myTransform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			myTransform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow) && onGround) 
+		{
+			myRigibody.velocity = new Vector3 (0, jumpSpeed, 0);
+		}
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Debug.Log ("Mouse Clicked on:"+Input.mousePosition);
+		}
+	}
+
+}
---
+++ Assets/KillSelf.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillSelf : MonoBehaviour {
+
+	public float KillTime;
+
+	private float Counter = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+		Counter += Time.deltaTime;
+
+		if (Counter > KillTime) 
+		{
+			GameObject.Destroy (this.gameObject);
+		}
+	
+	}
+}
